22828ea94e5dd29cd4d0fd394743d3369c65bd25
MensahVandyck <Mensahvandyck@gmail.com>
2022-04-19 15:08:05 +0000

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,16 +5,17 @@
  * Description-prints_putschar
  * return 0
  */
-int main (void)
+int main(void)
 {
-        char str[] = "_putchar";
-        int i = 0;
-        while (str[i] != '\0')
-        {
-                char c = str[i];
-                putchar(c);
-                i++;
-        }
-        putchar('\n');
+char str[] = "_putchar\n";
+int i = 0;
+
+while (str[i] != '\0')
+	{
+	char c = str[i];
+
+	_putchar(c);
+	i++;
+	}
 return (0);
 }
